a5024c712c9031850c2de024cb9e5ca5e542469f
yingquelou <2695193723@qq.com>
2021-08-23 10:47:39 +0800

    0823

--- Ovallearn.cc
+++ Ovallearn.cc
@@ -1,12 +1,21 @@
 #include "Oval.h"
+#include <math.h>
 int main(void)
 {
     // Oval t;
     Point A(1, -1), B(1, 1);
+    VectorOfMath p(6, 2, A);
+    // cout << "This point is " << Point(tan(M_PI / 3), 1).VectorMold() << endl;
+    cout << "This point is " << Point(0, sqrt(2.0)).CoordinateInBase(Point(1 / sqrt(2.0), 1 / sqrt(2.0)), Point(-1 / sqrt(2.0), 1 / sqrt(2.0))) << endl;
+    cout << Point(1, 1).CoordinateInBase(Point(0, 0), Point(0, 0)) << endl;
+    /* cout << "This point is " << A << B << endl;
+    cout << "A||B? " << (A || B ? "true" : "false") << endl;
     cout << "This point is " << A << endl;
-    A--;
+    --A-- --;
     cout << "This point is " << A << endl;
-    cout << "This point is " << A - B << endl;
+    cout << "A||B? " << (A || B ? "true" : "false") << endl;
+    cout << "This point is " << A << endl;
+    cout << "This point is " << A - B << endl; */
     // cout << A * B << endl;
     return 0;
 }
--- Point.h
+++ Point.h
@@ -1,23 +1,46 @@
 #pragma once
+#include <cmath>
+using std::sqrt;
 #include <iostream>
 using namespace std;
-// 向量/点类(数学)
+// 平面向量/点类(坐标表示)
 class Point
-{
-    friend ostream &operator<<(ostream &cout, const Point &P);
+{ //友元
 
-private:
+    //以 (横坐标,纵坐标) 形式输出坐标/向量,例如：(1,1)
+    friend ostream &operator<<(ostream &cout, const Point &P);
+    //向量的数乘(数前置版) 滑动向量的产生
+    friend Point operator*(const double &b, const Point &P);
+
+private: //成员变量
     double x;
     double y;
 
-public:
+public: //构造与析构
+    //无参构造
+    Point();
+    //拷贝构造
+    Point(const Point &point);
+    //有参构造1
+    Point(const double &, const double &);
+    //有参构造2
+    Point(const double &, const double &, const Point &P);
+    ~Point();
+
+public: //重载运算符
     //向量/坐标拷贝
     Point &operator=(const Point &b);
-    //向量的数量积
+    //向量的数量积(内积、点积)
     double operator*(const Point &b) const;
-    //点相加即对应坐标相加
+    //向量的数乘(数后置版) 滑动向量的产生
+    Point operator*(const double &b) const;
+    Point &operator*=(const Point &P);
+    //向量的合成与分解——合成
+    //点/向量相加即对应坐标相加
+    //向量的加法满足平行四边形法则和三角形法则
     Point operator+(const Point &P) const;
-    //点相减即对应坐标相减
+    //点/向量相减即对应坐标相减
+    //向量的减法满足三角形法则
     Point operator-(const Point &P) const;
     //前置递减为横坐标递减
     Point &operator--();
@@ -27,15 +50,85 @@ public:
     Point &operator++();
     //后置递增为纵坐标递增
     Point &operator++(int);
+    //判断点/向量相同
+    bool operator==(const Point &P) const;
+    //非零向量间判断平行
+    bool operator||(const Point &P) const;
 
-public:
-    //无参构造
-    Point();
-    //拷贝构造
-    Point(const Point &point);
-    Point(const double &, const double &);
-    ~Point();
+public: //成员函数
+    //滑动向量
+    Point Sliding(const double &, const double &, const Point &);
+    //向量的模
+    double VectorMold() const;
+    //向量的合成与分解——分解
+    //求当前向量在另一非零向量上的分量
+    Point VectorComponent(const Point &P) const;
+    //求当前向量在以P1和P2为基底的坐标系中的坐标表示
+    Point CoordinateInBase(const Point &P1, const Point &P2) const;
 };
+using VectorOfMath = Point;
+Point::Point() : x(0), y(0) {}
+Point::Point(const double &a, const double &b) : x(a), y(b) {}
+Point::Point(const Point &P) : x(P.x), y(P.y) {}
+Point::~Point() {}
+Point::Point(const double &a, const double &b, const Point &P)
+{
+    x = a * P.x;
+    y = b * P.y;
+}
+Point Point::operator*(const double &b) const
+{
+    return Point(b * x, b * y);
+}
+Point operator*(const double &b, const Point &P)
+{
+    return Point(b * P.x, b * P.y);
+}
+Point Point::CoordinateInBase(const Point &P1, const Point &P2) const
+{
+    Point P1_Component = this->VectorComponent(P1);
+    Point P2_Component = this->VectorComponent(P2);
+    double tx = P1_Component.VectorMold() / P1.VectorMold();
+    double ty = P2_Component.VectorMold() / P2.VectorMold();
+    if (*this * P1 < 0)
+        tx = -tx;
+    if (*this * P2 < 0)
+        ty = -ty;
+    return Point(tx, ty);
+}
+Point Point::VectorComponent(const Point &P) const
+{
+    double P_Mold = P.VectorMold();
+    if (!P_Mold)
+    //此时P为零向量，参数非法
+    {
+        cerr << "errer" << endl;
+        exit(EXIT_FAILURE);
+    }
+    double proportion = ((*this * P) / P_Mold) / P_Mold;
+    return proportion * P;
+}
+double Point::VectorMold() const
+{
+    return sqrt(x * x + y * y);
+}
+Point Point::Sliding(const double &a, const double &b, const Point &P)
+{
+    return Point(a * P.x, b * P.y);
+}
+bool Point::operator==(const Point &P) const
+{
+    if (x == P.x && y == P.y)
+        return true;
+    return false;
+}
+bool Point::operator||(const Point &P) const
+{
+    if (((*this == Point(0, 0)) + (P == Point(0, 0)) == 0) &&
+        x * P.y == y * P.x)
+        return true;
+    return false;
+}
 Point Point::operator+(const Point &P) const
 {
     return Point(x + P.x, y + P.y);
@@ -46,28 +139,24 @@ Point Point::operator-(const Point &P) const
 }
 Point &Point::operator++()
 {
-    x++;
+    ++x;
     return *this;
 }
 Point &Point::operator++(int)
 {
-    y++;
+    ++y;
     return *this;
 }
 Point &Point::operator--()
 {
-    x--;
+    --x;
     return *this;
 }
 Point &Point::operator--(int)
 {
-    y--;
+    --y;
     return *this;
 }
-Point::Point() : x(0), y(0) {}
-Point::Point(const double &a, const double &b) : x(a), y(b) {}
-Point::Point(const Point &point) : x(point.x), y(point.y) {}
-Point::~Point() {}
 double Point::operator*(const Point &b) const
 {
     return x * b.x + y * b.y;
